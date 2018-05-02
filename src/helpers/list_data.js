b2a18a239917171fd7b84e5ca52d755edd411ee3
So-Xtina <mu7zic2soul@gmail.com>
2018-05-02 16:23:31 -0700

    Completed displaying dummy data

---
+++ src/helpers/list_data.js
@@ -0,0 +1,18 @@
+export default [
+	{
+		title: "Learn React",
+		details: "Learn reactJS"
+	},
+	{
+		title: "Go to the Gym",
+		details: "Cardio day"
+	},
+	{
+		title: "Learn Node",
+		details: "Learn node for backend"
+	},
+	{
+		title: "Walk the Dog",
+		details: "Walk the dog to the park and play fetch"
+	}
+];
