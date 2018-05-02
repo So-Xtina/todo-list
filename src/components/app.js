563c272978f8976932a7538a4589895ba3986ce8
So-Xtina <mu7zic2soul@gmail.com>
2018-05-02 14:49:33 -0700

    initial commit

--- src/components/app.js
+++ src/components/app.js
@@ -1,14 +1,10 @@
-import React from 'react';
-import '../assets/css/app.css';
-import logo from '../assets/images/logo.svg';
+import React from "react";
+import "../assets/css/app.css";
 
 const App = () => (
-    <div>
-        <div className="app">
-            <img src={logo} className="logo rotate"/>
-            <h1>Welcome to React</h1>
-        </div>
-    </div>
+	<div>
+		<h1>To Do List</h1>
+	</div>
 );
 
 export default App;
